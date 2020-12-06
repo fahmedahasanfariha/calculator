512cd6dd9aeab120cf7d8ee27dcbabe58f1d7ade
Fahmeda Hasan Fariha <58398404+fahmedahasanfariha@users.noreply.github.com>
2020-12-07 02:12:35 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+/*function cal(){
+    var x =parseInt(document.getElementById("num1").value);
+    var y =parseInt(document.getElementById("num2").value);
+
+
+    var opt = document.getElementById("options").value;
+
+    if(opt === "+" ){
+    document.getElementById("result").value=x+y;
+    }
+    else if(opt === "-" ){
+    document.getElementById("result").value=x-y;
+    }
+    else if(opt === "*" ){
+    document.getElementById("result").value=x*y;
+    }
+    else if(opt === "/" ){
+    document.getElementById("result").value=x/y;
+    }
+    else{
+        document.getElementById("result").value="Worng Number";
+    }
+}*/
+/*
+<div class="container">
+        <h3 class="mb-3">A Simple calculator</h3>
+        <input type="text" placeholder="Enter number" class="formcontrol" id="num1">
+        <select name="" id="options">
+            <option value="0">Select</option>
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">x</option>
+            <option value="/">/</option>
+        </select>
+        <input type="text" placeholder="Enter number" class="formcontrol" id="num2">
+        <button class="btn btn-success" onclick="calculator()"><b>=</b></button>
+        <input type="text" class="formcontrol" id="result" placeholder="Result">
+    </div>
+    */
